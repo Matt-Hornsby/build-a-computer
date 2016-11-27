576ec492cdd9ede7f8a548a71fc9e7ede803ddfc
Hornsby, Matt <Matt.L.Hornsby@nordstrom.com>
2016-11-26 18:46:47 -0800

    Implemented large RAM chips and multiplication code

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -6,4 +6,25 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+    @R0           // Get the first operand from R0
+    D=M           // Place it in D
+    @i            // Go to the memory location for i
+    M=D           // Set i to the value we got from R0
+    @R2           // Go to the memory location for product
+    M=0           // Zero out the memory
+(LOOP)
+    @i            // Go back to loop counter address
+    D=M           // Store it in D
+    @END          // Get the address of the END label
+    D;JEQ         // Jump there if the loop counter reached 0
+    @R1           // Get value in R1
+    D=M           // Store it in D
+    @R2           // Go to the address of product
+    M=D+M         // Add existing product with R1
+    @i
+    M=M-1         // Decrement loop counter
+    @LOOP
+    0;JMP         // Go back to top of loop
+(END)
+    @END
+    0;JMP
